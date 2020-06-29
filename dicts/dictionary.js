d2ed915cb251087936d92b484522f9c71d7f816d
caixinquan <caixinquan@dfl.com.cn>
2020-06-29 15:52:44 +0800

    fix(dicts/dictionary.js): 修复环境变量读取失败问题

--- dicts/dictionary.js
+++ dicts/dictionary.js
@@ -1,6 +1,6 @@
 module.exports = {
   PROJECT_PATH: process.cwd(),
-  OPERATING_ENV: process.argv.slice(2),
+  OPERATING_ENV: process.argv.slice(2)[0],
   PLATFORM_DICT: {
     wx: 'wxss',
     swan: 'css',

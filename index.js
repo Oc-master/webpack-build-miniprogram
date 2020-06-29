bee76a585c0545ba45dcd0cdaecc39ef6af3e4cc
caixinquan <caixinquan@dfl.com.cn>
2020-06-29 13:51:54 +0800

    feat(dictionary.js): 添加字典管理文件

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const chalk = require('chalk');
 const yaml = require('js-yaml');
 
 const config = require('./webpack.config');
+const { PROJECT_PATH, OPERATING_ENV } = require('./dictionary');
 
-const PROJECT_PATH = process.cwd();
-const { OPERATING_ENV } = process.env;
+// const PROJECT_PATH = process.cwd();
+// const { OPERATING_ENV } = process.env;
 const YML = path.resolve(PROJECT_PATH, 'config.yaml');
 const obj = yaml.load(fs.readFileSync(YML, { encoding: 'utf-8' }));
-console.log(chalk.blue(obj));
+console.log(chalk.blue(obj), process.argv);
 
 OPERATING_ENV === 'production' && shell.rm('-rf', path.resolve(PROJECT_PATH, 'dist'));
 

6b1aab2bda3c889cf1bdb6b519a47f637dee0d68
caixinquan <caixinquan@dfl.com.cn>
2020-07-08 14:47:49 +0800

    feat(plugins/vant_extract_plugin.js): 完成构建依赖网络功能

--- plugins/vant_extract_plugin.js
+++ plugins/vant_extract_plugin.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const dayjs = require('dayjs');
 const chalk = require('chalk');
+const replaceExt = require('replace-ext');
 
 class VantExtractPlugin {
   constructor({ context, vantContext }) {
@@ -12,6 +13,12 @@ class VantExtractPlugin {
   apply(compiler) {
     compiler.hooks.entryOption.tap('VantExtractPlugin', () => {
       const initialEntries = this.getInitialEntry();
+      const fileSet = initialEntries.reduce((acc, item) => {
+        const temp = [];
+        this.createFileSet(this.context, item, temp);
+        return [...acc, ...temp];
+      }, []);
+      console.log(fileSet);
     });
   }
 
@@ -45,6 +52,28 @@ class VantExtractPlugin {
       return initialEntries.map((item) => this.convertToRelative(this.context, this.context, item));
     } catch(error) {
       console.log(chalk.gray(`[${dayjs().format('HH:mm:ss')}]`), chalk.red('ERROR: "app.json" 文件内容读取失败'));
+    }
+  }
+
+  createFileSet(context, relativePath, fileSet) {
+    const absolutePath = path.resolve(context, replaceExt(relativePath, '.json'));
+    const isQualification = fs.existsSync(absolutePath);
+    if (!isQualification) return undefined;
+    fileSet.push(absolutePath);
+    try {
+      const content = fs.readFileSync(absolutePath, { encoding: 'utf-8' });
+      const { usingComponents = {} } = JSON.parse(content);
+      const components = Object.values(usingComponents);
+      const { length: componentsLength } = components;
+      if (componentsLength) {
+        const moduleContext = path.dirname(absolutePath);
+        components.forEach((component) => {
+          const relativePath = this.convertToRelative(this.context, moduleContext, component);
+          this.createFileSet(this.context, relativePath, fileSet);
+        });
+      }
+    } catch(e) {
+      console.log(chalk.gray(`[${dayjs().format('HH:mm:ss')}]`), chalk.red(`ERROR: "${absolutePath}" 文件内容读取失败`));
     }
   }
 }

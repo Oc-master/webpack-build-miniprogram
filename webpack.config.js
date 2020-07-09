20359be7e6f25df1213b610a60352d9f5bd7e82d
caixinquan <caixinquan@dfl.com.cn>
2020-07-09 19:52:15 +0800

    feat(webpack.config.js): babel处理js过程中增加@babel/plugin-proposal-class-properties插件运作

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,11 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           // presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-transform-modules-commonjs'], // '@babel/plugin-transform-runtime'
+          plugins: [
+            // '@babel/plugin-transform-runtime',
+            '@babel/plugin-transform-modules-commonjs',
+            "@babel/plugin-proposal-class-properties", { "loose": true },
+          ],
         },
       },
       {

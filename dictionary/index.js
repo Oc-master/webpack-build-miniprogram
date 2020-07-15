ff27c24b0e0c88c95814659b596d88d3c819cf01
caixinquan <caixinquan@dfl.com.cn>
2020-07-15 15:53:51 +0800

    feat(dictionary/index.js): 添加SOURCE DESTINATION常量

--- dictionary/index.js
+++ dictionary/index.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
+const PROJECT_PATH = process.cwd();
+const [OPERATING_ENV] = process.argv.slice(2);
+const SOURCE = path.resolve(PROJECT_PATH, 'src');
+const DESTINATION = path.resolve(PROJECT_PATH, 'dist');
+
 module.exports = {
-  PROJECT_PATH: process.cwd(),
-  OPERATING_ENV: process.argv.slice(2)[0],
+  PROJECT_PATH,
+  OPERATING_ENV,
+  SOURCE,
+  DESTINATION,
   PLATFORM_DICT: {
     template: {
       wx: 'wxml',

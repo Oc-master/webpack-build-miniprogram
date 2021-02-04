798ae10f74f22e742ecf8e025cb32f1f14fa3eb4
caixinquan <caixinquan@dfl.com.cn>
2021-02-04 14:42:51 +0800

    feat(libs/index.js): 分解MS_ROUTES常量,减小构建体积

--- libs/index.js
+++ libs/index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
+const chalk = require('chalk');
+const dayjs = require('dayjs');
 const yaml = require('js-yaml');
 
 const { CONFIG, DEFAULT_CONFIG, APP_CONFIG } = require('./dicts');
+
+const warningLog = (message) => console.log(chalk.gray(`[${dayjs().format('HH:mm:ss')}]`), chalk.yellow(`WARNING: ${message}`));
+
+const errorLog = (message) => console.log(chalk.gray(`[${dayjs().format('HH:mm:ss')}]`), chalk.red(`ERROR: ${message}`));
 
 exports.yamlConfig = (function () {
   try {
@@ -58,5 +64,42 @@ exports.packages = (function () {
     return packages;
   } catch (error) {
     return [];
+  }
+})();
+
+exports.routesConfig = (function () {
+  try {
+    const routesConfig = {};
+    const content = fs.readFileSync(APP_CONFIG, { encoding: 'utf-8' });
+    const { pages = [], subpackages = [] } = JSON.parse(content);
+    const mainPackageRoutes = pages.reduce((acc, page) => {
+      const arr = page.split('/');
+      const key = arr[arr.length - 2];
+      if (acc[key]) {
+        warningLog(`"${page}" 路由键值 ${key} 已被 "${acc[key]}" 占用`);
+        return acc;
+      }
+      return { ...acc, [key]: page };
+    }, {});
+    Object.assign(routesConfig, { MS_ROUTES: JSON.stringify(mainPackageRoutes) });
+    const { length: subpackagesLength } = subpackages;
+    if (!subpackagesLength) return routesConfig;
+    subpackages.forEach((item) => {
+      const { root, pages } = item;
+      if (!root) return undefined;
+      const subPackageRoutes = pages.reduce((acc, page) => {
+        const arr = page.split('/');
+        const key = arr[arr.length - 2];
+        if (acc[key]) {
+          warningLog(`${root} 分包中 "${page}" 路由键值 ${key} 已被 "${acc[key]}" 占用`);
+          return acc;
+        }
+        return { ...acc, [key]: page };
+      }, {});
+      Object.assign(routesConfig, { [`MS_ROUTES.${root}`]: JSON.stringify(subPackageRoutes) });
+    });
+    return routesConfig;
+  } catch (err) {
+    errorLog(err);
   }
 })();
